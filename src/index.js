b1edeb80b14845c55d57addffdfe0539e39f47b0
Diana Zakirova <zakirovadiana.mk@gmail.com>
2020-06-14 16:29:10 +0300

    create horizontal scroll

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './index.scss'
+
+// горизонтальный скролл
+if(document.documentElement.clientWidth > 1024){
+    window.addEventListener('wheel', e => window.scrollBy(e.deltaY, 0))
+}
---
+++ src/script/scroll.js
@@ -0,0 +1,15 @@
+export function verticalScroll (event){
+    if (event.deltaMode == event.DOM_DELTA_PIXEL) {
+        var modifier = 1;
+        // иные режимы возможны в Firefox
+    } else if (event.deltaMode == event.DOM_DELTA_LINE) {
+        var modifier = parseInt(getComputedStyle(this).lineHeight);
+    } else if (event.deltaMode == event.DOM_DELTA_PAGE) {
+        var modifier = this.clientHeight;
+    }
+    if (event.deltaY != 0) {
+        // замена вертикальной прокрутки горизонтальной
+        this.scrollLeft += modifier * event.deltaY;
+        event.preventDefault();
+    }
+}
